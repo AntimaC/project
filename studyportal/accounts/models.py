13b84b6782d647e5567f63b28d282da8a1607ebb
AntimaC <87909369+AntimaC@users.noreply.github.com>
2022-06-04 17:31:05 +0530

    Update models.py

--- studyportal/accounts/models.py
+++ studyportal/accounts/models.py
@@ -8,6 +8,11 @@ from django.contrib.auth.models import User
 from django.utils.timezone import now
 # Create your models here.
 
+class Register(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    branch= models.CharField(max_length=100)
+    def __str__(self):
+       return self.user.username
 
 class Profile(models.Model):
     user = models.OneToOneField(User,default="1", on_delete=models.CASCADE, related_name="profile")
